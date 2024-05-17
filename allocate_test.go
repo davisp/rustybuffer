c63c9731001eb0144e4c531fae52a75dd932f8fb
Paul J. Davis <paul.davis@tiledb.com>
2024-05-17 18:03:25 -0500

    Add a super complicated benchmark

--- allocate_test.go
+++ allocate_test.go
@@ -6,3 +6,12 @@ func ExampleAllocBuffers() {
 	entry := AllocBuffers(sizes[:])
 	entry.Release()
 }
+
+func AllocBuffersSpeed() {
+	Configure(8*1024*1024*1024, 2*1024*1024*1024)
+	sizes := [...]uint64{256*1024*1024}
+	for i := 0; i < 10_000; i++ {
+		entry := AllocBuffers(sizes[:])
+		entry.Release()
+	}
+}
