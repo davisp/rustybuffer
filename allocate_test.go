692f4705426faee5624c02895c49b25c9bf6b002
Stefan Naglee <stefannaglee@gmail.com>
2024-05-17 16:21:15 -0400

    Make an importable package, move main to example

---
+++ allocate_test.go
@@ -0,0 +1,16 @@
+package allocate
+
+import (
+	"fmt"
+)
+
+func ExampleAllocBuffers() {
+	Configure(8*1024*1024*1024, 2*1024*1024*1024)
+	sizes := [...]uint64{5, 10, 15}
+	entry := AllocBuffers(sizes[:])
+	fmt.Println("[Go]: Entry:", entry)
+	// Output: Allocating buffers: [5 10 15]
+	// Golang total bytes: 30
+	// Entry: {0x600001ab4000 [[0 0 0 0 0] [0 0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]]}
+	entry.Release()
+}
